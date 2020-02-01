964ec780e079a95b557dbcc1e5e85645bb906e0d
jgierlach <jegierlach@gmail.com>
2020-02-01 10:27:39 -0500

    Delete components that are no longer used

--- client/pages/Home.vue
+++ client/pages/Home.vue
@@ -2,19 +2,9 @@
   <div class="my-32">
     <h1 class="text-center text-brown" v-if="!auth">Mevn Auth</h1>
     <h1 class="text-center" v-else>Welcome, {{ user.name }}</h1>
-    <GenerateQuote />
-    <AvailableContacts />
   </div>
 </template>
 
 <script>
-import GenerateQuote from '../components/GenerateQuote.vue'
-import AvailableContacts from '../components/AvailableContacts.vue'
-
-export default {
-  components: {
-    GenerateQuote,
-    AvailableContacts
-  }
-}
+export default {}
 </script>
